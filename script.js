bc23fa89a7ccaff71dc1a0146048b4fa1060a6e0
Vaishali Tomar <vaishalitomar830@gmail.com>
2024-02-28 13:10:55 +0530

    file added

---
+++ script.js
@@ -0,0 +1,10 @@
+let result = document.getElementById("result");
+console.log(result);
+result.style.color = "blue";
+result.style.backgroundColor = "yellow";
+result.style.margin = "20px";
+result.style.padding = "20px";
+result.style.fontSize = "18px";
+result.style.fontWeight = "bold";
+result.style.height = "200px";
+result.style.width = "300px";
